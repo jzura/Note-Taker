1ef608e950b74b1bd757562c564492b02a2d133c
Stitch <john@comteque.com.au>
2020-07-25 17:56:40 +0800

    new files

---
+++ server.js
@@ -0,0 +1,77 @@
+  
+const express = require("express")
+const path = require("path");
+const fs = require("fs");
+
+const staticDir = path.resolve(__dirname, "public");
+const dbDir = path.resolve(__dirname, "db");
+const dbFile = path.join(dbDir, "db.json");
+
+
+// Sets up the Express server
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express server to handle data parsing and serve static files
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(express.static(staticDir));
+
+// Routes
+server.get("/", function (request, response) {
+    response.sendFile(path.join(staticDir, "index.html"));
+});
+
+server.get("/notes", function (request, response) {
+    response.sendFile(path.join(staticDir, "notes.html"));
+});
+
+server.get("/api/notes", function (request, response) {
+    fs.readFile(dbFile, 'utf8', (error, data) => {
+        if (error) throw error;
+        if (data) {
+            response.json(JSON.parse(data));
+        } else {
+            response.json([]);
+        }
+
+    });
+});
+
+
+server.post("/api/notes", (request, response) => {
+    const currentNote = { title: request.body.title, text: request.body.text };
+    fs.readFile(dbFile, 'utf8', (error, data) => {
+        if (error) throw error;
+        let savedNotes = data ? JSON.parse(data) : [];
+        savedNotes.push(currentNote);
+        savedNotes = savedNotes.map((note, index) => {
+            note.id = index + 1;
+            return note;
+        });
+        fs.writeFile(dbFile, JSON.stringify(savedNotes), error => {
+            if (error) throw error;
+            console.log(`New note with id = ${savedNotes.length} saved successfully`);
+            response.json(currentNote);
+        });
+    });
+});
+
+server.delete("/api/notes/:id", (request, response) => {
+    fs.readFile(dbFile, 'utf8', (error, data) => {
+        if (error) throw error;
+        let savedNotes = data ? JSON.parse(data) : [];
+        savedNotes = savedNotes.filter((note, index) => {
+            return note.id != request.params.id;
+        });
+        fs.writeFile(dbFile, JSON.stringify(savedNotes), error => {
+            if (error) throw error;
+            response.end();
+        });
+    });
+});
+
+// Starts the server to begin listening
+server.listen(PORT, function () {
+    console.log("server listening on PORT " + PORT);
+});
